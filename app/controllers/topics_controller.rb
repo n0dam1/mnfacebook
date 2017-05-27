e6c82cf89dc98b2ac08960bf8e6e2d82a620e707
minoru_noda <minoru.noda.0527@gmail.com>
2017-05-27 21:08:17 +0900

    add
    add bootstrap

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,12 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
   def index
     @topics = Topic.all
   end
 
   def create
-    @topic = Topic.create(topic_params)
+    @topic = Topic.create(topics_params)
     redirect_to topics_path
   end
 
@@ -12,9 +14,26 @@ class TopicsController < ApplicationController
     @topic = Topic.new
   end
 
+  def edit
+  end
+
+  def update
+    @topic.update(topics_params)
+    redirect_to topics_path
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path
+  end
+
   private
-    def topic_params
+    def topics_params
       params.require(:topic).permit(:content)
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:id])
     end
 
 end
