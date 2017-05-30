dcab1570650cc0862f4744b5f6cab3a027f012bb
minoru_noda <minoru.noda.0527@gmail.com>
2017-05-30 21:11:33 +0900

    fix
    colon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers {
+  devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy]
