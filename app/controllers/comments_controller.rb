654f548235516e96879fd5a315ab7a8557da171e
minoru_noda <minoru.noda.0527@gmail.com>
2017-06-01 14:13:07 +0900

    add
    comment ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,46 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only:[:destroy, :edit, :update]
+
   def create
     @comment = current_user.comments.build(comment_params)
-    @topic = @comment.topic
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to topic_path(@topic), notice: 'コメントを投稿しました' }
+        format.js { render :index }
       else
-        format.html { render :new }
+        format.html { render :index }
       end
     end
   end
+
+  def destroy
+    respond_to do |format|
+      if @comment.destroy
+        format.js { render :index }
+      else
+        format.html { render :index }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @topic = @comment.topic
+    @comment.update(comment_params)
+    redirect_to topic_path(@topic), notice: "コメントを更新しました！"
+  end
+
 
   private
 
   def comment_params
     params.require(:comment).permit(:topic_id, :content)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
 end
